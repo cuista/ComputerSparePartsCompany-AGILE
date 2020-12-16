c92c48cc45075a509f16767e6d83f61a17289e93
Squame4 <squame4@gmail.com>
2020-12-16 11:50:54 +0100

    Correzione Controller Customer

--- src/main/java/it/unical/asd/group6/computerSparePartsCompany/data/controller/CustomerController.java
+++ src/main/java/it/unical/asd/group6/computerSparePartsCompany/data/controller/CustomerController.java
@@ -4,10 +4,7 @@ import it.unical.asd.group6.computerSparePartsCompany.data.entities.Customer;
 import it.unical.asd.group6.computerSparePartsCompany.data.services.implemented.CustomerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customer")
@@ -22,7 +19,7 @@ public class CustomerController {
         return ResponseEntity.ok(customerService.checkLogin(username,password));
     }
 
-    @GetMapping("/register")
+    @PostMapping("/register")
     public ResponseEntity<Boolean> doSignUp (
             @RequestParam String name, @RequestParam String surname,
             @RequestParam String phoneNumber, @RequestParam String email,
