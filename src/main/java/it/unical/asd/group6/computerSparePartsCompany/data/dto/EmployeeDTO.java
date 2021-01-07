bb62c10e8d0e9000ba9c050e355191bea23b6bda
cuista <carlo.cristarellaorestano@gmail.com>
2021-01-07 19:42:11 +0100

    created EmployeeDTO and ProductionHouseDTO classes

---
+++ src/main/java/it/unical/asd/group6/computerSparePartsCompany/data/dto/EmployeeDTO.java
@@ -0,0 +1,101 @@
+package it.unical.asd.group6.computerSparePartsCompany.data.dto;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class EmployeeDTO implements Serializable {
+
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String firstname;
+
+    private String lastname;
+
+    private LocalDate hiringDate;
+
+    private String email;
+
+    private String telephoneNumber;
+
+    public EmployeeDTO() {}
+
+    public EmployeeDTO(Long id, String username, String password, String firstname, String lastname, LocalDate hiringDate, String email, String telephoneNumber) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.hiringDate = hiringDate;
+        this.email = email;
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public LocalDate getHiringDate() {
+        return hiringDate;
+    }
+
+    public void setHiringDate(LocalDate hiringDate) {
+        this.hiringDate = hiringDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephone_number) {
+        this.telephoneNumber = telephone_number;
+    }
+
+}
